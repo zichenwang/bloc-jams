6e1532e82077496839fef95e1ea0b8e2cb7a5d40
Zichen Wang <wangzichen530@hotmail.com>
2016-10-16 15:05:19 -0500

    9 assignment: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,16 +2,18 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function (points) {
 
-    var revealPoint = function (index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function (point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
     };
 
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+    forEach(points, revealPoint);
+
+    //    for (var i = 0; i < points.length; i++) {
+    //        revealPoint(i);
+    //    }
 
 };
 
